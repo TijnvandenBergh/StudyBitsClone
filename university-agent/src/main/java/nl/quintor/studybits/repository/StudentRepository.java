8ba29cad4f6a29e84edc3911902f852d763559fa
Tijn167 <tijnvandenbergh2@hotmail.com>
2019-07-01 11:35:17 +0200

    Changes to config.yml for jacoco

--- university-agent/src/main/java/nl/quintor/studybits/repository/StudentRepository.java
+++ university-agent/src/main/java/nl/quintor/studybits/repository/StudentRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
+public interface  StudentRepository extends JpaRepository<Student, Long> {
     public Student getStudentByStudentId(String studentId);
     public Student getStudentByStudentDid(String studentDid);
 }
